d3f3ed3edc5599f6cbc3204e6d70e70e1a04b839
mnboy19 <jchrisboy0@gmail.com>
2021-04-10 20:40:39 -0500

    my first commit

---
+++ my_shopping_database/productdb.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 11, 2021 at 01:26 AM
+-- Server version: 8.0.21
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `productdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `producttb`
+--
+
+DROP TABLE IF EXISTS `producttb`;
+CREATE TABLE IF NOT EXISTS `producttb` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(25) NOT NULL,
+  `product_price` float DEFAULT NULL,
+  `product_image` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `producttb`
+--
+
+INSERT INTO `producttb` (`id`, `product_name`, `product_price`, `product_image`) VALUES
+(1, 'Apple Macbook Pro', 1799, './upload/product1.png'),
+(2, 'Sony E7 Headphones', 147, './upload/product2.png'),
+(3, 'Sony Xperia Z4', 459, './upload/product3.png'),
+(4, 'Samsung Galaxy A50', 278, './upload/product4.png');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
